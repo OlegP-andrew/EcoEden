79026820fbf0a09da260711a0e32c2771ffabadf
Adeline09 <xianwu@andrew.cmu.edu>
2024-02-21 17:09:14 -0500

    player controller movement fixed

--- Unity_EcoEden/Assets/PlayerController.cs
+++ Unity_EcoEden/Assets/PlayerController.cs
@@ -7,6 +7,13 @@ public class PlayerController : MonoBehaviour
     InputAction moveAction; 
     // InputAction rotateAction;
 
+    public CharacterController controller;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public Transform cam;
+    private float targetAngle;
+
+
     private Vector2 direction;
     [SerializeField] float speed = 5.0f;
     [SerializeField] float rotationSpeed = 700f;
@@ -31,15 +38,14 @@ public class PlayerController : MonoBehaviour
 
     private void RotatePlayer()
     {
-        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-        Quaternion targetRotation = Quaternion.Euler(0f, angle, 0f);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * smooth);
-
+        targetAngle = Mathf.Atan2(-1 * direction.y, direction.x) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
     }
 
     private void MovePlayer()
     {
-        Vector2 direction = moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.y, 0, -1 * direction.x) * speed * Time.deltaTime;
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        controller.Move(moveDir.normalized * speed * Time.deltaTime);
     }
 }
