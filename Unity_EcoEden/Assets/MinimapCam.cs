79f8a4bef002683dfc9962a2f2c351c1697501d0
Adeline09 <xianwu@andrew.cmu.edu>
2024-04-03 20:22:25 -0400

    simple minimap done

---
+++ Unity_EcoEden/Assets/MinimapCam.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class MinimapCam : MonoBehaviour {
+
+	// public Shader unlitShader;
+
+	// void Start() {
+	// 	unlitShader = Shader.Find("Unlit/Texture");
+	// 	GetComponent<Camera>().SetReplacementShader(unlitShader,"");
+	// }
+}
