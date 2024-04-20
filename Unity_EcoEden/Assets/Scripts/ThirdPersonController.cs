7989dbc889786dbcf35574c23a166a1e72201118
Adeline09 <xianwu@andrew.cmu.edu>
2024-04-20 00:43:20 -0400

    restoring changes

--- Unity_EcoEden/Assets/Scripts/ThirdPersonController.cs
+++ Unity_EcoEden/Assets/Scripts/ThirdPersonController.cs
@@ -20,6 +20,8 @@ namespace StarterAssets
 
         [Tooltip("Sprint speed of the character in m/s")]
         public float SprintSpeed = 5.335f;
+        public bool isSprinting;
+        public bool isDriving;
         public bool LockCameraPosition = false;
 
         [Tooltip("How fast the character turns to face movement direction")]
@@ -232,7 +234,7 @@ namespace StarterAssets
             }
 
             // Update PlantBuddyDriving FMOD Instance
-            SoundManager.S.PlantBuddyDriveUpdate(isDriving, isSprinting);
+            // SoundManager.S.PlantBuddyDriveUpdate(isDriving, isSprinting);
             
             // a reference to the players current horizontal velocity
             float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
