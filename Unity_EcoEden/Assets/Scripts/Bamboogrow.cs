4843bc3404f9e3480f549b1923f6ba20283b8f3e
Adeline09 <xianwu@andrew.cmu.edu>
2024-04-25 23:14:02 -0400

    fixed a lot of things

--- Unity_EcoEden/Assets/Scripts/Bamboogrow.cs
+++ Unity_EcoEden/Assets/Scripts/Bamboogrow.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class BambooGrow : MonoBehaviour
 {
-    public GameObject bamboo;
-
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Seed")
         {
-            bamboo.GetComponent<Animator>().SetTrigger("grow");
+            GetComponent<Animator>().SetTrigger("grow");
         }
     }
 }
