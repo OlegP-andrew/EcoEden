792666b478c0d715a58b13c6cea418717614456e
Adeline09 <xianwu@andrew.cmu.edu>
2024-04-25 21:50:41 -0400

    river almost done

--- Unity_EcoEden/Assets/Scripts/BoatCollisionDetection.cs
+++ Unity_EcoEden/Assets/Scripts/BoatCollisionDetection.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class BoatCollisionDetection : MonoBehaviour
 {
     public LeafBoat leafBoat;
-    
+    private bool counter;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            leafBoat.StartOff();
+            if (!counter) leafBoat.StartOff();
         }
+        counter = true;
     }
 }
--- Unity_EcoEden/Assets/Scripts/LeafBoat.cs
+++ Unity_EcoEden/Assets/Scripts/LeafBoat.cs
@@ -1,21 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarterAssets;
 
 public class LeafBoat : MonoBehaviour
 {
-    private Animator anim;
-    
+    public Transform plantBuddy;
     public GameObject player;
+
+    private ThirdPersonController thirdPersonController;
+    private Animator anim;
+    private bool constraint;
+    private Vector3 positionOffset;
 
     public void Start()
     {
         anim = GetComponent<Animator>();
+        thirdPersonController = player.GetComponent<ThirdPersonController>();
+    }
+    public void Update()
+    {
+        if (constraint) 
+        {
+            transform.position = plantBuddy.position + positionOffset;
+        }
     }
 
     public void StartOff()
     {
-        anim.SetTrigger("boat");
+        StartCoroutine(PrepareBoat());
+    }
 
+    public IEnumerator PrepareBoat()
+    {
+        thirdPersonController.enabled = false;
+
+        anim.SetTrigger("boat");
+        yield return new WaitForSeconds(1f);
+
+        positionOffset = transform.position - plantBuddy.position;
+
+        anim.SetTrigger("go");
+        yield return new WaitForSeconds(2f);
+
+        constraint = true;
+        thirdPersonController.MoveSpeed /= 2;
+        thirdPersonController.enabled = true;
+
+        yield return new WaitForSeconds(20f);
     }
 }
