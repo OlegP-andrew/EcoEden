d418c8577041f4cd9e32b491fd9f6f124a3121cd
Adeline09 <xianwu@andrew.cmu.edu>
2024-04-25 22:34:26 -0400

    did boat

--- Unity_EcoEden/Assets/Scripts/LeafBoat.cs
+++ Unity_EcoEden/Assets/Scripts/LeafBoat.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
+using UnityEngine.SceneManagement;
 
 public class LeafBoat : MonoBehaviour
 {
     public Transform plantBuddy;
+    public GameObject plantBuddy1;
     public GameObject player;
 
     private ThirdPersonController thirdPersonController;
     private Animator anim;
     private bool constraint;
     private Vector3 positionOffset;
+    // private BoxCollider buddyCollider;
 
     public void Start()
     {
         anim = GetComponent<Animator>();
         thirdPersonController = player.GetComponent<ThirdPersonController>();
+        // buddyCollider = plantBuddy1.GetComponent<BoxCollider>();
     }
+
     public void Update()
     {
         if (constraint) 
@@ -44,9 +49,14 @@ public class LeafBoat : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
         constraint = true;
-        thirdPersonController.MoveSpeed /= 2;
+        thirdPersonController.MoveSpeed /= 1.5f;
+        thirdPersonController.JumpHeight = 0f;
+        // buddyCollider.size = new Vector3(0.2f, 0.142f, 0.26f);
         thirdPersonController.enabled = true;
 
-        yield return new WaitForSeconds(20f);
+        yield return new WaitForSeconds(60f);
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
