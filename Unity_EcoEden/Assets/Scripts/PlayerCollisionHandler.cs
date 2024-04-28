02d9318ae615b80f811fbf23d13f9f7b973bb7ae
Adeline09 <xianwu@andrew.cmu.edu>
2024-04-27 20:05:54 -0400

    Revert "fix"
    This reverts commit ef6be55dc08fcd562ea471f3bfddd612388409e1.

--- Unity_EcoEden/Assets/Scripts/PlayerCollisionHandler.cs
+++ Unity_EcoEden/Assets/Scripts/PlayerCollisionHandler.cs
@@ -1,51 +1,14 @@
-using System;
-using System.Linq;
-using FMODUnityResonance;
-using NUnit.Framework.Constraints;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
     [SerializeField] float bounceForce;
 
-    public LayerMask usethis;
-    private string surfaceMaterial;
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Fog"))
         {
             // TODO: Play animation
-        }
-    }
-
-    void Start()
-    {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        Collider[] myColliders = Physics.OverlapSphere(transform.position, 0.5f, usethis);
-        if (myColliders != null && myColliders.Length != 0)
-        {
-            switch (myColliders[0].tag)
-            {
-                case "water":
-                    surfaceMaterial = "Water";
-                    break;
-                case "dirt":
-                    surfaceMaterial = "Dirt";
-                    break;
-                case "stone":
-                    surfaceMaterial = "Stone";
-                    break;
-                default:
-                    surfaceMaterial = "Grass";
-                    break;
-            }
-
         }
     }
 }
