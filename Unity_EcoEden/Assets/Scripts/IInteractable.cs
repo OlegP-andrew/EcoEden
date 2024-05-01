1c220b6dd37d7798f00b442206d8d2f0992f3776
OlegP-andrew <143562498+OlegP-andrew@users.noreply.github.com>
2024-05-01 16:06:23 -0400

    Prefab for examinable

--- Unity_EcoEden/Assets/Scripts/IInteractable.cs
+++ Unity_EcoEden/Assets/Scripts/IInteractable.cs
@@ -5,15 +5,19 @@ using cakeslice;
 
 public abstract class IInteractable: MonoBehaviour
 {
+
+    public GameObject hint;
     public abstract void Interact();
 
     public void HighlightOn()
     {
+        if (hint != null) hint.SetActive(true);
         gameObject.GetComponent<Outline>().enabled = true;
     }
 
     public void HighlightOff()
     {
+        if (hint != null) hint.SetActive(false);
         gameObject.GetComponent<Outline>().enabled = false;
     }
 }
